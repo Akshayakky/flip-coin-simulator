4ae692236b28d5dc998b7bbde9d3f360f997d880
akshayakky <akshaysmpt01@gmail.com>
2020-02-18 10:05:33 +0530

    Flip Coin Multiple Times And Find Percent Of Singlet Combination

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -2,14 +2,60 @@
 
 #CONSTANTS
 IS_HEAD=0
+SINGLET=1
 
-#VARIABLES
-randomFlip=$((RANDOM%2))
+#DECLARE DICTIONARY
+declare -A combinationDictionary
 
-#CHECKING FOR HEAD OR TAIL
-if [ $randomFlip -eq $IS_HEAD ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+#FUNCTION TO SIMULATE SINGLET COMBINATION
+function flipCoin(){
+	local NO_OF_COINS=$2
+	for (( index=1; index<=$1; index++ ))
+	do
+		for (( coins=1; coins<=$NO_OF_COINS; coins++ ))
+		do
+			randomFlip=$((RANDOM%2))
+			if [ $randomFlip -eq $IS_HEAD ]
+			then
+				flipCombination=$flipCombination"H"
+			else
+				flipCombination=$flipCombination"T"
+			fi
+		done
+
+		#STORING HEAD AND TAIL COMBINATION COUNT IN COMBINATION DICTIONARY
+		((combinationDictionary[$flipCombination]++))
+		flipCombination=""
+	done
+}
+
+#FUNCTION TO CONVERT DICTIONARY TO ARRAY OF COMBINATIONS AND COUNTS
+function dictionaryToArray(){
+	local combinationAndCountArray=("$@")
+	local NO_OF_RECORDS=$((${#combinationAndCountArray[@]}/2))
+	for (( i=0; i<$NO_OF_RECORDS; i++ ))
+	do
+		newCombinationAndCountArray[$i]=${combinationAndCountArray[$i]}:${combinationAndCountArray[$NO_OF_RECORDS + $i]}
+	done
+	echo ${newCombinationAndCountArray[@]}
+}
+
+#CONVERTING COUNT TO PERCENTAGE IN COMBINATION ARRAY
+function calculatePercentage(){
+	local combinationAndCountArray=("$@")
+	for i in ${!combinationAndCountArray[@]}
+	do
+		key=$(echo ${combinationAndCountArray[$i]} | cut -f 1 -d ":")
+		count=$(echo ${combinationAndCountArray[$i]} | cut -f 2 -d ":")
+		combinationAndPercentArray[$i]=$key:`echo "scale=2; $count*100/$numberIteration" | bc`
+	done
+	echo ${combinationAndPercentArray[@]}
+}
+
+#READING NO OF ITERATION FROM USER
+read -p "Enter No of Iteration : " numberIteration 
+
+flipCoin $numberIteration $SINGLET
+
+combinationArray=($(dictionaryToArray ${!combinationDictionary[@]} ${combinationDictionary[@]}))
+combinationArray=($(calculatePercentage ${combinationArray[@]}))
